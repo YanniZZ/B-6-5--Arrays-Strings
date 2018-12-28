4e09d31d7e599f23e9082246a6ae2688408b1f6c
Ян Золоторевич <yanzolotorevich@gmail.com>
2018-12-28 20:28:52 +0300

    Practice on lesson

--- B-6-Arrays-Strings/Program.cs
+++ B-6-Arrays-Strings/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,78 @@ namespace Base.Lesson_6
     {
         static void Main(string[] args)
         {
-            Pyatnashki();
+            //int[] six = new[] { 1,2,3,4,5,6};
+            //for (int i = six.Length-1; i >= 0; i--) { Console.Write(six[i]); }
+            //Console.WriteLine(string.Join(", ", six));
+            Random rnd = new Random();
+            int[,] six2 = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    six2[i, j] = rnd.Next(100);
+                }
+            }
+            //for (int i = 0; i < six2.Rank; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.WriteLine(six2.GetUpperBound(i));
+            //    for (int j = 0; j < six2.GetUpperBound(i); j++)
+            //    {
+            //        six2[i, j] = rnd.Next(100);
+            //        Console.WriteLine($"{six2[i,j], 5}");
+            //    }
+            //}
+            outmass(six2);
+            //poemGeneration();
             Console.ReadLine();
         }
 
+        
+
+        //int temp = 0;
+        //for (int i =0; i < six2.GetLenght(0); i++)
+        //{
+        //    for (int j = 0; j < six2.GetLenght(0); j++)
+        //    if (six2[i,j]>= temp)
+        //    {
+        //        temp = six2[i, j];
+        //    }
+        //    Console.WriteLine(temp);
+        //}
+        public static void poemGeneration()
+        {
+            Random rnd = new Random();
+            char[][] poem = new char [100][];
+            for (int i = 0; i <= 99; i++)
+            {
+                var word = new char[rnd.Next(10)];
+                for (int j = 0; j<word.Length;j++)
+                {
+                    word[j] = Convert.ToChar(rnd.Next(1040 - 1, 1103));
+                }
+                poem[i] = word;
+            }
+
+            foreach (var word in poem)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        public static void outmass(int [,] six2)
+        {
+            //for (int i = 0; i < (six2.GetUpperBound(0)+1); i++)
+            //{
+            //    for (int j = 0; j < six2.Length/(six2.GetUpperBound(0)+1); j++)
+            //    {
+            //        Console.WriteLine($"{six2[i,j], 5}");
+            //    }
+            //}
+            foreach (var element in six2)
+            {
+                Console.WriteLine(element);
+            }
+        }
         public static void Pyatnashki()
         {
             var numbers = GetInitialValues();
